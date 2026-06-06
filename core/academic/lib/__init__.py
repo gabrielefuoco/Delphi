7d6ff0b98269037a546c1e695b7b273e3c8b6565
gabriele.fuoco <gabriele.fuoco99@gmail.com>
2026-06-06 18:33:30 +0200

    chore: remove superfluous code from core/academic/lib

--- core/academic/lib/__init__.py
+++ core/academic/lib/__init__.py
@@ -4,5 +4,3 @@ from .compiler import Compiler
 from .bib_parser import BibParser
 from .paths import get_pandoc_exe, get_typst_exe
 from .citation_service import BibliographyService
-from .logger import setup_logger
-from .html_renderer import HTMLRenderer
